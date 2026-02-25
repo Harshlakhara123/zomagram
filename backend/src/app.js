21eae9022a4860792013693d2983abcb952a465d
Harshlakhara123 <lakharaharsh65@gmail.com>
2026-02-26 02:32:14 +0530

    api creation for user , partners and foods

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.routes");
+const foodRoutes = require("./routes/food.routes");
 
 const app = express();
 app.use(cookieParser());
@@ -12,6 +13,8 @@ app.get("/", (req,res) => {
     res.send("hello");
 })
 
+
 app.use("/api/auth", authRoutes);
+app.use("/api/food", foodRoutes);
 
 module.exports = app;
